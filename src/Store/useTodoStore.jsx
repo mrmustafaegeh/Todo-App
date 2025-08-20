4e03f5fc4ce4658c2ed7322da7fde5e836c4e373
mustafa egeh <Mr.mustafaegeh@gmail.com>
2025-08-20 14:22:25 +0300

    fixed2

--- src/Store/useTodoStore.jsx
+++ src/Store/useTodoStore.jsx
@@ -1,9 +1,20 @@
 import { create } from "zustand";
 import { persist, devtools } from "zustand/middleware";
 
-const useTodoStore = create((set)=>{
-    todos: [],
-    addToDoos: (todo) => set((state) => ({ todos: [...state.todos, todo] })),
-    deleteTodo: (todo) => set((state) => ({ todos: state.todos.filter((val) => val !== todo) })),   
-    completeTodo: (todo) => set((state) => ({ todos: state.todos.map((val) => val === todo ? {...val, complete: !val.complete} : val) })),  
-})
+const useTodoStore = create((set) => ({
+  todos: [],
+  addTodo: (todo) =>
+    set((state) => ({
+      todos: [...state.todos, todo],
+    })),
+  deleteTodo: (todo) =>
+    set((state) => ({
+      todos: state.todos.filter((val) => val !== todo),
+    })),
+  completeTodo: (todo) =>
+    set((state) => ({
+      todos: state.todos.map((val) =>
+        val === todo ? { ...val, complete: !val.complete } : val
+      ),
+    })),
+}));
